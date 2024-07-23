af1a79fc2faa770e1e8e165e1f5a01fa03b5d01f
lamia-elazzouzi-cs <lamia.elazzouzi.cs@gmail.com>
2024-07-23 07:09:56 -0400

    Implement Task5

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -2,7 +2,10 @@ let books = {
     1: {
         "author": "Chinua Achebe",
         "title": "Things Fall Apart",
-        "reviews": {}
+        "reviews": {
+            1: "nada",
+            2: "hello"
+        }
     },
     2: { "author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {} },
     3: { "author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {} },
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -59,8 +59,15 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+        const reviews = book.reviews;
+        res.send(JSON.stringify(reviews, null, 4));
+    }
+    else {
+        return res.status(404).json({ message: "Can't find any book with that isbn." });
+    }
 });
 
 module.exports.general = public_users;
