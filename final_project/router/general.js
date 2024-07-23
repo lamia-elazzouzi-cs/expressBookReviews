0cf35c30898ed36cb6b725f156785accbc940dab
lamia-elazzouzi-cs <lamia.elazzouzi.cs@gmail.com>
2024-07-23 06:37:10 -0400

    Implement Task2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,15 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+
+    const isbn = req.params.isbn;
+    let book = books[isbn];
+    if (book) {
+        res.send(JSON.stringify(book, null, 4));
+    } else {
+        return res.status(404)
+            .json({ message: "The requested book does not exist" });
+    }
 });
 
 // Get book details based on author
