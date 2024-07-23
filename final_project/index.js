45e78831414187abd4da09739654676b7e551c9f
lamia-elazzouzi-cs <lamia.elazzouzi.cs@gmail.com>
2024-07-23 06:24:01 -0400

    Add session authorization feature

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,39 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+    "/customer",
+    session({
+        secret: "fingerprint_customer",
+        resave: true,
+        saveUninitialized: true
+    }));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) {
+        // if the user is logged in, create token
+        let token = req.session.authorization["accessToken"];
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json(
+                    { message: "User not authenticated. Please register or log in." }
+                );
+            }
+        });
+    } else {
+        // the user is not logged in
+        return res.status(403).json(
+            { message: "The user is not logged in!" }
+        );
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
